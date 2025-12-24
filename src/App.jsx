044abdc697d71c123977e95e73272d134e9fc0be
Edip Hazuri <edip@medip.dev>
2025-12-24 23:29:51 +0300

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import ComingSoon from './components/ComingSoon'
 import MainSite from './components/MainSite'
 
